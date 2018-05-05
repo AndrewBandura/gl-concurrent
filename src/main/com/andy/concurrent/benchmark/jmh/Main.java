d18d42c481b48ce0777b4f4d15d8457a2b6bb389
andrew.bandura@gmail.com <a1n9d7y7>
2018-05-05 23:51:36 +0300

    -refactored

--- src/main/com/andy/concurrent/benchmark/jmh/Main.java
+++ src/main/com/andy/concurrent/benchmark/jmh/Main.java
@@ -5,7 +5,6 @@ import org.openjdk.jmh.runner.RunnerException;
 public class Main {
 
     public static void main(String[] args) throws RunnerException {
-        //IncrementBenchmark.runTests();
         MultiThreadingIncrementBenchmark multiThreadingIncrementBenchmark = new MultiThreadingIncrementBenchmark();
         multiThreadingIncrementBenchmark.runTests();
     }
